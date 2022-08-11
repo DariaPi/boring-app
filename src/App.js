51fbcfc9e4f9e2aeb866f160057269e0c8b9f9b3
DariaPi <100636273+DariaPi@users.noreply.github.com>
2022-08-11 22:00:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [tip, setTip] = useState ("");
 
   const getTip = async () => {
-    const responce = await fetch ('http://www.boredapi.com/api/activity/ ');
+    const responce = await fetch ('http://www.boredapi.com/api/activity/');
     const data = await responce.json ();
     setTip (data.activity);
   }
